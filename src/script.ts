10a522d6ffa1a643b7566aa5be979e4f00ff77f3
mjh-shikder <mjhsikder@gmail.com>
2026-07-12 23:44:35 +0600

    post query added

--- src/script.ts
+++ src/script.ts
@@ -1,25 +1,36 @@
+import { title } from "node:process";
 import { prisma } from "./lib/prisma";
 
 async function main() {
-  //? Create a new user with a post    
-    const user = await prisma.user.create({
-        data: {
-            name: "mjh",
-            email: "mjh@mial.com",
-            posts: {
-                create: {
-                    title: "first post ",
-                    content: "this is the first post!",
-                    published: true,
-                }
-            }
-        },
-        include: {
-            posts: true,
-        }
-    })
+  //? Create a new user with a post
+  // const user = await prisma.user.create({
+  //     data: {
+  //         name: "mjh 2",
+  //         email: "mjh@mial2.com",
+  //         posts: {
+  //             create: {
+  //                 title: "second post ",
+  //                 content: "this is the second post!",
+  //                 published: true,
+  //             }
+  //         }
+  //     },
+  //     include: {
+  //         posts: true,
+  //     }
+  // })
 
-  console.log("Created user:", user);
+  // ? create a new post
+  const post = await prisma.post.create({
+    data: {
+      authorId: 1,
+      title: "id 1's post",
+      content: "this is a new post!",
+      published: true,
+    },
+  });
+
+  console.log("Created user:", post);
 
   // Fetch all users with their posts
   const allUsers = await prisma.user.findMany({
